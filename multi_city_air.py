5335a95cefc0a2a911c2b308efe2abd623d221a4
Nevenka Lukic <nenamilkov@gmail.com>
2024-03-19 19:18:47 +0100

    created utils script and modified air and weather scripts

--- multi_city_air.py
+++ multi_city_air.py
@@ -1,57 +1,6 @@
-import openmeteo_requests
-import requests_cache
-import requests
 import pandas as pd
-from retry_requests import retry
-import json
+from utils import *
 
-def fetch_air_quality_data(url, params):
-    """
-    Function to fetch weather data from the Open-Meteo API.
-
-    Parameters:
-        url (str): The base URL for the Open-Meteo API.
-        params (list): A list of dictionaries, each containing parameters for a specific location.
-
-    Returns:
-        list: A list of responses containing air quality data for each location.
-    """
-    # Setup the Open-Meteo API client with cache and retry on error
-    cache_session = requests_cache.CachedSession('.cache', expire_after=3600)      # Cache session to avoid redundant requests
-    retry_session = retry(cache_session, retries=5, backoff_factor=0.2)            # Retry failed requests
-    openmeteo = openmeteo_requests.Client(session=retry_session)                   # Initialize the Open-Meteo API client
-
-    # Fetch air quality data for each location
-    responses = [openmeteo.weather_api(url, params=p) for p in params]
-    
-    return responses
-
-def get_city_name(latitude, longitude):
-    """
-    Function to retrieve the city name based on latitude and longitude using reverse geocoding.
-
-    Parameters:
-        latitude (float): The latitude coordinate of the location.
-        longitude (float): The longitude coordinate of the location.
-
-    Returns:
-        str: The name of the city corresponding to the provided coordinates.
-    """
-    # Reverse geocoding with OpenStreetMap Nominatim
-    url = "https://nominatim.openstreetmap.org/reverse"     # Base URL for reverse geocoding service
-    params = {
-        "format": "json",
-        "lat": latitude,
-        "lon": longitude
-    }
-    response = requests.get(url, params=params)             # Send a GET request to the reverse geocoding service
-    if response.status_code == 200:
-        data = response.json()
-        city_name = data.get('address', {}).get('city', 'Unknown')      # Extract city name from the response data
-        return city_name
-    else:
-        print(f"Failed to retrieve city name for coordinates ({latitude}, {longitude})")
-        return "Unknown"
 
 def process_air_quality_data(responses):
     """
@@ -66,8 +15,8 @@ def process_air_quality_data(responses):
     all_data = []
     for response in responses:
         # Process air quality data for each location
-        response = response[0]      # Select the first location from the response list
-        city_name = get_city_name(response.Latitude(), response.Longitude())        # Retrieve city name
+        response = response[0]
+        city_name = get_city_name(response.Latitude(), response.Longitude())
         city_data = {
             "city": city_name,
             "city": f"{response.Latitude()}°N {response.Longitude()}°E",
@@ -96,57 +45,27 @@ def process_air_quality_data(responses):
                 "european_aqi": response.Hourly().Variables(12).ValuesAsNumpy().tolist()
             }
         }
-        all_data.append(city_data)          # Append processed data for each location to the list
+        all_data.append(city_data)
     
     return all_data
 
-def write_to_files(data, json_filename, csv_filename):
-    """
-    Function to write processed air quality data to JSON and CSV files.
-
-    Parameters:
-        data (list): Processed air quality data for all locations.
-        json_filename (str): Filename for the JSON output file.
-        csv_filename (str): Filename for the CSV output file.
-    """
-    # Write to JSON file
-    with open(json_filename, 'w') as json_file:
-        json.dump(data, json_file)
-
-    # Combine data for all cities into a single DataFrame
-    combined_data = []
-    for city_data in data:
-        city_hourly_data = city_data.pop("hourly_data")
-        city_df = pd.DataFrame(data=city_hourly_data)
-        city_df['city'] = city_data['city']             # Add city name to DataFrame
-        combined_data.append(city_df)
-    combined_df = pd.concat(combined_data, ignore_index=True)
-
-    # Write combined DataFrame to a CSV file
-    combined_df.to_csv(csv_filename, index=False)
-
-    print(f"Data written to {json_filename} and {csv_filename}")
-
-# Selecting cities from different parts of Spain to observe potential differences in air quality, particularly in pollen levels
-cities = [
-    {"latitude": 36.7202, "longitude": -4.4203},  # Malaga
-    {"latitude": 40.4165, "longitude": -3.7026},  # Madrid
-    {"latitude": 43.2627, "longitude": -2.9253},  # Bilbao
-    {"latitude": 41.3888, "longitude": 2.159},    # Barcelona
-    {"latitude": 42.6, "longitude": -5.5703,}     # León
-
-    # Add more cities as needed
-]
 
 url = "https://air-quality-api.open-meteo.com/v1/air-quality"
+
+# Parameters for fetching air quality data
 params = [{"latitude": city["latitude"], "longitude": city["longitude"],
         "hourly": ["pm10", "pm2_5", "dust", "uv_index", "uv_index_clear_sky", "ammonia", "alder_pollen",
                     "birch_pollen", "grass_pollen", "mugwort_pollen", "olive_pollen", "ragweed_pollen",
                     "european_aqi"],
         "timezone": "Europe/Berlin",
         "forecast_days": 7}
-        for city in cities]         # Parameters for fetching air quality data
+        for city in cities]         
 
-responses = fetch_air_quality_data(url, params)         # Fetch weather forecast data
-all_data = process_air_quality_data(responses)          # Process weather forecast data
-write_to_files(all_data, 'multi_city_air_quality.json', 'multi_city_air_quality.csv')       # Write data to files
+# Fetch weather forecast data
+responses = fetch_openmeteo_data(url, params)         
+
+# Process weather forecast data
+all_data = process_air_quality_data(responses)          
+
+# Write data to files
+write_to_files(all_data, 'multi_city_air_quality.json', 'multi_city_air_quality.csv')       
---
+++ utils.py
@@ -0,0 +1,105 @@
+import openmeteo_requests
+import requests_cache
+import requests
+import pandas as pd
+from retry_requests import retry
+import json
+
+# Selecting cities from different parts of Spain to observe potential variations 
+# in weather patterns
+# Additional cities from different regions can be added for further analysis
+cities = [
+    {"latitude": 36.7202, "longitude": -4.4203},  # Malaga (Southern Spain)
+    {"latitude": 40.4165, "longitude": -3.7026},  # Madrid (Central Spain)
+    {"latitude": 43.2627, "longitude": -2.9253},  # Bilbao (Northern Spain)
+    {"latitude": 41.3888, "longitude": 2.159},    # Barcelona (Eastern Spain)
+    {"latitude": 42.6, "longitude": -5.5703,}     # León (Northwestern Spain)
+    
+]
+
+def fetch_openmeteo_data(url, params):
+    """
+    Function to fetch data from the Open-Meteo API.
+
+    Parameters:
+        url (str): The base URL for different the Open-Meteo APIs: weather, air quality etc. 
+        params (list): A list of dictionaries, each containing parameters for a specific location.
+
+    Returns:
+        list: A list of responses containing data for each location.
+    """
+    # Setup the Open-Meteo API client with cache and retry on error
+    # Cache session to avoid redundant requests
+    cache_session = requests_cache.CachedSession('.cache', expire_after=3600)       
+    
+    # Retry failed requests
+    retry_session = retry(cache_session, retries=5, backoff_factor=0.2)             
+    
+    # Initialize the Open-Meteo API client
+    openmeteo = openmeteo_requests.Client(session=retry_session)                    
+
+    # Fetch weather forecast data for each location
+    responses = [openmeteo.weather_api(url, params=p) for p in params]
+    
+    return responses
+
+def get_city_name(latitude, longitude):
+    """
+    Function to retrieve the city name based on latitude and longitude using reverse geocoding.
+
+    Parameters:
+        latitude (float): The latitude coordinate of the location.
+        longitude (float): The longitude coordinate of the location.
+
+    Returns:
+        str: The name of the city corresponding to the provided coordinates.
+    """
+    # Reverse geocoding with OpenStreetMap Nominatim
+    # Base URL for reverse geocoding service
+    url = "https://nominatim.openstreetmap.org/reverse"         
+    params = {
+        "format": "json",
+        "lat": latitude,
+        "lon": longitude
+    }
+    
+    # Send a GET request to the reverse geocoding service
+    response = requests.get(url, params=params)                 
+    if response.status_code == 200:
+        data = response.json()
+        
+        # Extract city name from the response data
+        city_name = data.get('address', {}).get('city', 'Unknown')      
+        return city_name
+    else:
+        print(f"Failed to retrieve city name for coordinates ({latitude}, {longitude})")
+        return "Unknown"
+
+def write_to_files(data, json_filename, csv_filename):
+    """
+    Function to write processed weather forecast data to JSON and CSV files.
+
+    Parameters:
+        data (list): Processed weather forecast data for all locations.
+        json_filename (str): Filename for the JSON output file.
+        csv_filename (str): Filename for the CSV output file.
+    """
+    # Write to JSON file
+    with open(json_filename, 'w') as json_file:
+        json.dump(data, json_file)
+
+    # Combine data for all cities into a single DataFrame
+    combined_data = []
+    for city_data in data:
+        city_hourly_data = city_data.pop("hourly_data")
+        city_df = pd.DataFrame(data=city_hourly_data)
+        
+        # Add city name to DataFrame
+        city_df['city'] = city_data['city']             
+        combined_data.append(city_df)
+    combined_df = pd.concat(combined_data, ignore_index=True)
+
+    # Write combined DataFrame to a CSV file
+    combined_df.to_csv(csv_filename, index=False)
+
+    print(f"Data written to {json_filename} and {csv_filename}")            
